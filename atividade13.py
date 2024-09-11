030c913415a89ab442f01c81f853a32a5710a3d8
AnaKaroline-23 <anakarolinesoraes.23@gmail.com>
2024-09-11 16:00:49 -0300

    programa que informa se o número do usuário é positivo, negativo ou neutro

---
+++ atividade13.py
@@ -0,0 +1,11 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+numero = float(input(" número do usuário "))
+if (numero<0):
+    print("Negativo")
+
+elif (numero == 0):
+    print("Neutro")
+
+elif (numero >0):
+    print("Positivo")
+    
